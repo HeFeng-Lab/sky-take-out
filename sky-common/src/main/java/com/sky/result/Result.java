262bf7127e1f7dffb5db1760d2ca4c8fd1f00f53
hefeng6500 <2443992009@qq.com>
2023-08-25 14:53:51 +0800

    feat: 添加response结果封装

---
+++ sky-common/src/main/java/com/sky/result/Result.java
@@ -0,0 +1,35 @@
+package com.sky.result;
+
+
+import lombok.Data;
+import java.io.Serializable;
+
+@Data
+public class Result<T> implements Serializable {
+  private Integer code;
+  private String msg;
+  private T data;
+
+  public static <T> Result<T> success() {
+    Result<T> result = new Result<T>();
+    result.code = 1;
+    result.msg = "success";
+    return result;
+  }
+
+  public static <T> Result<T> success(T object) {
+    Result<T> result = new Result<T>();
+    result.code = 1;
+    result.data = object;
+    result.msg = "success";
+    return result;
+  }
+
+  public static <T> Result<T> error(String msg) {
+    Result result = new Result();
+    result.code = 0;
+    result.msg = msg;
+
+    return result;
+  }
+}
