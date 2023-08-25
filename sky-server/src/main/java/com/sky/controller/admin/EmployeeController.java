db884ef0b82319e635fd6f9966e40daf97c556b8
hefeng6500 <2443992009@qq.com>
2023-08-25 14:54:10 +0800

    fix: 完善登录功能

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -3,11 +3,13 @@ package com.sky.controller.admin;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import com.sky.result.Result;
 import com.sky.service.EmployeeService;
 import com.sky.vo.EmployeeLoginVO;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,16 +24,29 @@ public class EmployeeController {
   EmployeeService employeeService;
 
   @PostMapping("/login")
-  public EmployeeLoginVO login(HttpServletRequest request, @RequestBody EmployeeLoginDTO employeeLoginDTO) {
+  public Result<EmployeeLoginVO> login(HttpServletRequest request, @RequestBody EmployeeLoginDTO employeeLoginDTO) {
     log.info("员工登录： {}", employeeLoginDTO);
 
     LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-
     queryWrapper.eq(Employee::getUsername, employeeLoginDTO.getUsername());
+    // queryWrapper.eq(Employee::getPassword, employeeLoginDTO.getPassword());
 
     Employee employee = employeeService.getOne(queryWrapper);
 
-    log.info(String.valueOf(employee));
+    // 未查到账户
+    if (employee == null) {
+      return Result.error("登录失败");
+    }
+
+    // 密码不匹配
+    if(!employee.getPassword().equals(employeeLoginDTO.getPassword())) {
+      return Result.error("登录失败");
+    }
+
+    // 账户已禁用
+    if(employee.getStatus() == 0) {
+      return Result.error("账户已禁用");
+    }
 
     request.getSession().setAttribute("employee", employee.getId());
 
@@ -41,6 +56,6 @@ public class EmployeeController {
             .name(employee.getName())
             .build();
 
-    return employeeLoginVO;
+    return Result.success(employeeLoginVO);
   }
 }
